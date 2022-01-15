a9d91489dea7dedeed3f14335d448b0917e2660d
srinikhil99 <srinikhilreddy99@gmail.com>
2022-01-15 20:32:20 +0530

    add dic-4

--- dic2.js
+++ dic2.js
@@ -4,33 +4,33 @@ const wrapper = document.querySelector(".dic_wrapper"),
     infoText = wrapper.querySelector(".dic_info-text"),
     volumeIcon = wrapper.querySelector(".dic_word i"),
     removeIcon = wrapper.querySelector(".dic_search span");
-    let audio;
+// let audio;
 
 //data function
 function data(result, dic_word) {
     if (result.title) {
         //if api returns the message of pass by word
         infoText.innerHTML = `Can't find the meaning of <span>"${dic_word}"</span>. Please, check your spelling or try to search with another word`;
-    }else{
-        console.log(result);
+    } else {
+        // console.log(result);
         wrapper.classList.add("active");
-        let definitions = result[0].meaning[0];
+        let definitions = result[0];
         // phonetics = `${result[0].meanings[0].partOfSpeech} / ${result[0].phonetics[0].text}/`;
 
         //passing the particular response data to a particular html element
         document.querySelector(".dic_word p").innerText = result[0].word;
         // document.querySelector(".dic_word span").innerText = phonetics;
-        document.querySelector(".dic_meaning span").innerText = definitions.firstOccurence;
-        document.querySelector(".dic_example span").innerText = definitions.example;
+        document.querySelector(".dic_meaning span").innerText = definitions.meaning;
+        document.querySelector(".dic_example span").innerText = definitions.firstOccurence;
         // audio = new Audio("https:" + result[0].phonetics[0].audio); //creating new audio object and passing audio src
 
-        if(definitions.relatedLinks[0] == undefined){ //if there is no synonym then hide the synonym div
+        if (definitions.relatedLinks[0] == undefined) { //if there is no synonym then hide the synonym div
             relatedLinks.parentElement.style.display = "none";
-        }else{
+        } else {
             relatedLinks.parentElement.style.display = "block";
             relatedLinks.innerHTML = "";
             for (let i = 0; i < 5; i++) { //getting only 5 relatedLinks out of many
-                let tag = `<span onclick=search('${definitions.relatedLinks[i]}')>${definitions.relatedLinks[i]},</span>`; 
+                let tag = `<span onclick=search('${definitions.relatedLinks[i]}')><a href="https://${definitions.relatedLinks[i]}" target="_blank">${definitions.relatedLinks[i]}</a>,</span>`;
                 relatedLinks.insertAdjacentHTML("beforeend", tag); //passing all 5 relatedLinks inside relatedLinks           
             }
         }
@@ -38,10 +38,10 @@ function data(result, dic_word) {
 }
 
 //search synonym word
-function search(word){
-    searchInput.value = word;
-    fetchApi(word);
-}
+// function search(word){
+//     searchInput.value = word;
+//     fetchApi(word);
+// }
 
 //fetch api function
 function fetchApi(dic_word) {
@@ -60,11 +60,11 @@ searchInput.addEventListener("keyup", e => {
     }
 });
 
-volumeIcon.addEventListener("click", ()=>{
+volumeIcon.addEventListener("click", () => {
     audio.play();
 });
 
-removeIcon.addEventListener("click", ()=>{
+removeIcon.addEventListener("click", () => {
     searchInput.value = "";
     searchInput.focus();
     wrapper.classList.remove("active");
